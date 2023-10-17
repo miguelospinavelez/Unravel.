2c071783aa4569e76f4a9ce4a49109815e0e304c
Miguel Ospina <miguel.ospina.vz@hotmail.com>
2023-10-17 20:01:48 +0000

    Fixes: Main

--- app/home/main.php
+++ app/home/main.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['user_id'])) {
     <title> Unravel. | Microdosing & Mushroom Cultivation </title>
 
     <!-- favicon link -->
-    <link rel="icon" type="image/x-icon" href="/imgs/icons/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="../imgs/icons/favicon.ico">
 
     <!-- swiper css link -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
@@ -25,7 +25,7 @@ if (isset($_SESSION['user_id'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!--custom css-->
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
@@ -34,12 +34,12 @@ if (isset($_SESSION['user_id'])) {
 
     <section class="header">
 
-        <a href="home.php" class="logo">Unravel.</a>
+        <a href="./home.php" class="logo">Unravel.</a>
         <nav class="navbar">
             <a style="color:blueviolet; pointer-events: none;">home</a>
-            <a href="about.php">about</a>
-            <a href="package.php">archive</a>
-            <a href="book.php">products</a>
+            <a href="../about/about.php">about</a>
+            <a href="../archive/package.php">archive</a>
+            <a href="../products/book.php">products</a>
             <a id="loginButton" style="cursor: pointer;">Log in</a>
         </nav>
         <div id="menu-btn" class="fas fa-bars"></div>
@@ -57,27 +57,27 @@ if (isset($_SESSION['user_id'])) {
 
             <div class="swiper-wrapper">
 
-                <div class="swiper-slide slide" style="background-image: url(/imgs/fungi.jpg);">
+                <div class="swiper-slide slide" style="background-image: url(../imgs/fungi.jpg);">
                     <div class="content">
                         <span>discover, heal, connect</span>
                         <h3>integrate the power of mushrooms into your life</h3>
-                        <a href="book.php" class="btn">discover more</a>
+                        <a href="../products/book.php" class="btn">discover more</a>
                     </div>
                 </div>
 
-                <div class="swiper-slide slide" style="background-image:url(/imgs/heal.jpg)">
+                <div class="swiper-slide slide" style="background-image:url(../imgs/heal.jpg)">
                     <div class="content">
                         <span>discover, heal, connect</span>
                         <h3>unravel your self</h3>
-                        <a href="book.php" class="btn">discover more</a>
+                        <a href="../products/book.php" class="btn">discover more</a>
                     </div>
                 </div>
 
-                <div class="swiper-slide slide" style="background-image:url(/imgs/couple-cover-image-holding-hands.jpg)">
+                <div class="swiper-slide slide" style="background-image:url(../imgs/couple-cover-image-holding-hands.jpg)">
                     <div class="content">
                         <span>discover, heal, connect</span>
                         <h3>connect with those you love</h3>
-                        <a href="book.php" class="btn">discover more</a>
+                        <a href="../products/book.php" class="btn">discover more</a>
                     </div>
                 </div>
 
@@ -101,22 +101,22 @@ if (isset($_SESSION['user_id'])) {
 
         <div class="box-container">
             <div class="box">
-                <img src="imgs/" alt="">
+                <img src="" alt="">
                 <h3>Explore</h3>
             </div>
 
             <div class="box">
-                <img src="imgs/" alt="">
+                <img src="" alt="">
                 <h3>Heal</h3>
             </div>
 
             <div class="box">
-                <img src="imgs/" alt="">
+                <img src="" alt="">
                 <h3>Connect</h3>
             </div>
 
             <div class="box">
-                <img src="imgs/" alt="">
+                <img src="" alt="">
                 <h3>Grow</h3>
             </div>
 
@@ -132,7 +132,7 @@ if (isset($_SESSION['user_id'])) {
         <div class="content">
             <h3>about us</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus corrupti quod ipsam totam, itaque vero vel aut optio ad harum laudantium? Voluptatem architecto eaque veniam nisi, voluptates fuga dignissimos praesentium illum eveniet. Minima sit expedita dolorum excepturi similique nam, maiores, accusamus tempore eveniet molestiae facilis. Facere possimus sit accusantium at.</p>
-            <a href="about.php" class="btn">read more</a>
+            <a href="../about/about.php" class="btn">read more</a>
         </div>
     </section>
 
@@ -146,51 +146,51 @@ if (isset($_SESSION['user_id'])) {
 
             <div class="box">
                 <div class="image">
-                    <img src="imgs/micr.jpg" alt="">
+                    <img src="../imgs/micr.jpg" alt="">
                 </div>
                 <div class="content">
                     <h3>Introduction to microdosing</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eveniet sequi omnis ipsam exercitationem, corrupti illo quis rerum facere! Voluptatum quibusdam debitis facere magni sunt error voluptas beatae commodi optio.</p>
-                    <a href="package.php" class="btn">Read</a>
+                    <a href="../archive/package.php" class="btn">Read</a>
                 </div>
             </div>
 
             <div class="box">
                 <div class="image">
-                    <img src="imgs/labs.jpg" alt="">
+                    <img src="../imgs/labs.jpg" alt="">
                 </div>
                 <div class="content">
                     <h3>Studies on Psilocibin</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eveniet sequi omnis ipsam exercitationem, corrupti illo quis rerum facere! Voluptatum quibusdam debitis facere magni sunt error voluptas beatae commodi optio.</p>
-                    <a href="package.php" class="btn">Read</a>
+                    <a href="../archive/package.php" class="btn">Read</a>
                 </div>
             </div>
 
             <div class="box">
                 <div class="image">
-                    <img src="imgs/meditation.jpg" alt="">
+                    <img src="../imgs/meditation.jpg" alt="">
                 </div>
                 <div class="content">
                     <h3>Meditation Vault</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eveniet sequi omnis ipsam exercitationem, corrupti illo quis rerum facere! Voluptatum quibusdam debitis facere magni sunt error voluptas beatae commodi optio.</p>
-                    <a href="package.php" class="btn">Read</a>
+                    <a href="../archive/package.php" class="btn">Read</a>
                 </div>
             </div>
 
             <div class="box">
                 <div class="image">
-                    <img src="imgs/mushrms.jpg" alt="">
+                    <img src="../imgs/mushrms.jpg" alt="">
                 </div>
                 <div class="content">
                     <h3>mushrooms & fungi</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eveniet sequi omnis ipsam exercitationem, corrupti illo quis rerum facere! Voluptatum quibusdam debitis facere magni sunt error voluptas beatae commodi optio.</p>
-                    <a href="package.php" class="btn">Read</a>
+                    <a href="../archive/package.php" class="btn">Read</a>
                 </div>
             </div>
 
         </div>
 
-        <div class="load-more"><a href="package.php" class="btn">load more</a></div>
+        <div class="load-more"><a href="../archive/package.php" class="btn">load more</a></div>
 
     </section>
 
@@ -201,9 +201,9 @@ if (isset($_SESSION['user_id'])) {
     <section class="home-offer">
         <div class="content">
             <h3>Grow your own mushrooms</h3>
-            <img src="/imgs/growbox.png" alt="">
+            <img src="../imgs/growbox.png" alt="">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero vel voluptatem ea nam maxime porro deserunt aspernatur voluptatibus, quia odio.</p>
-            <a href="book.php" class="btn">Starter Kit</a>
+            <a href="../products/book.php" class="btn">Starter Kit</a>
         </div>
     </section>
 
@@ -217,16 +217,16 @@ if (isset($_SESSION['user_id'])) {
 
             <div class="box">
                 <h3>quick links</h3>
-                <a href="home.php"> <i class="fas fa-angle-rig"></i>home</a>
-                <a href="about.php"> <i class="fas fa-angle-rig"></i>about</a>
-                <a href="package.php"> <i class="fas fa-angle-rig"></i>archive</a>
-                <a href="book.php"> <i class="fas fa-angle-rig"></i>products</a>
+                <a href="./home.php"> <i class="fas fa-angle-rig"></i>home</a>
+                <a href="../about/about.php"> <i class="fas fa-angle-rig"></i>about</a>
+                <a href="../archive/package.php"> <i class="fas fa-angle-rig"></i>archive</a>
+                <a href="../products/book.php"> <i class="fas fa-angle-rig"></i>products</a>
             </div>
 
             <div class="box">
                 <h3>extra links</h3>
                 <a href="#"> <i class="fas fa-angle-right"></i>ask questions</a>
-                <a href="contact.php"> <i class="fas fa-angle-right"></i>contact us</a>
+                <a href="#"> <i class="fas fa-angle-right"></i>contact us</a>
                 <a href="#"> <i class="fas fa-angle-right"></i>privacy policy</a>
                 <a href="#"> <i class="fas fa-angle-right"></i>terms of use</a>
             </div>
@@ -373,7 +373,7 @@ if (isset($_SESSION['user_id'])) {
 
 
     <!-- custom js file link -->
-    <script src="js/script.js"></script>
+    <script src="../js/script.js"></script>
 
 
 
